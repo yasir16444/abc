ddfadc0572873c6ec696b5eb8ff4b503325b71cb
yasir16444 <yasir.rehman164@gmail.com>
2022-03-15 22:08:37 +0500

    my first learning of python

---
+++ main.py
@@ -0,0 +1,34 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings
+from typing import List
+
+numbers = [1,36,6,9,36,100,9,10,10]
+unique = []
+for x in numbers:
+    if x not in unique:
+        unique.append(x)
+print(unique)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
